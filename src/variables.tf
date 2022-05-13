ae0ce71fc2c5162a9c0a5e27b507e9ca3cfa6067
Joel Barna <joel.tbarna@gmail.com>
2022-05-12 22:43:14 -0400

    removed time resources and optional time variables

--- src/variables.tf
+++ src/variables.tf
@@ -1,8 +1,5 @@
 variable candidates {
     type = map(object({
-        starting_time = optional(string)
-        duration = optional(string)
-
         interviewers = map(object({
             arn = string
         }))
